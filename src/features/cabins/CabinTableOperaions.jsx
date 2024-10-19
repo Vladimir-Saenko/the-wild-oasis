2d577de67fe8c85bb1f1083676186c69178c1b84
Vladimir Saenko <siborg.451@gmail.com>
2024-10-19 18:55:07 +0700

    111

--- src/features/cabins/CabinTableOperaions.jsx
+++ src/features/cabins/CabinTableOperaions.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperaions() {
   return (
@@ -10,6 +11,12 @@ function CabinTableOperaions() {
           { label: "All", value: "all" },
           { label: "No discount", value: "no-discount" },
           { label: "With discount", value: "with-discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name=asc", label: "Sort by name (A-Z)" },
+          { value: "name=desc", label: "Sort by name (Z-A)" },
         ]}
       />
     </TableOperations>
