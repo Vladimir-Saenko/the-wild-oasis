1b1e0384ae6212802ca6cf8d384b88cefb710159
Vladimir Saenko <siborg.451@gmail.com>
2024-12-18 11:15:06 +0700

    Добавлено отображение статистики в Dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,13 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
+
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +15,39 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+
+  const { isLoading: isLoadingCabins, data: cabins } = useQuery({
+    queryKey: ["cabins"],
+    queryFn: getCabins,
+  });
+
+  const numCabins = cabins?.length || 0;
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins)
+    return <Spinner />;
+
+  //
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        numCabins={numCabins}
+      />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
