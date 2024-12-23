49a9e5ebc59af744765cf324948c35705027673e
Vladimir Saenko <siborg.451@gmail.com>
2024-12-23 11:09:14 +0700

    Чистка аккаунта по умолчанию

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("siborg@test.com");
-  const [password, setPassword] = useState("siborg");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
